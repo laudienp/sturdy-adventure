b984c58479fbd575c5698c7ecd3c0c77c6d5c509
laudienp <paul.laudien@etu.unice.fr>
2021-02-04 18:39:45 +0100

    add first weapon with bullet

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 10;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("hit!");
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/WeaponController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponController : MonoBehaviour
+{
+
+    public float firerate = 0.5f;
+    public GameObject projectilePrefab;
+    public Transform projectileSpawnPoint;
+
+    float lastShot;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButton(0) && lastShot < Time.time)
+        {
+            Instantiate(projectilePrefab, projectileSpawnPoint.position, Quaternion.LookRotation(projectileSpawnPoint.forward));
+            lastShot = Time.time + firerate;
+        }
+    }
+}
