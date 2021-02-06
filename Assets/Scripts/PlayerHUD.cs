c848aee12726c574a19d2753505210256ee54bb9
laudienp <paul.laudien@etu.unice.fr>
2021-02-06 20:05:33 +0100

    add fx package for future effects

---
+++ Assets/Scripts/PlayerHUD.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHUD : MonoBehaviour
+{
+    public Text healthText;
+
+    public Health playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth.onHit += UpdateHealth;
+        playerHealth.onDie += ResetHealth;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void UpdateHealth()
+    {
+        healthText.text = "HP : " + playerHealth.health;
+    }
+
+    void ResetHealth()
+    {
+        healthText.text = "HP : " + playerHealth.maxHealth;
+    }
+}
