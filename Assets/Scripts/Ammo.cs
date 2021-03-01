e075f0129a79990e10112677fc43ff8e5a7f5026
laudienp <paul.laudien@etu.unice.fr>
2021-03-01 13:15:00 +0100

    add weapon manager for swap between weapon + fix pickable ammo

--- Assets/Scripts/Ammo.cs
+++ Assets/Scripts/Ammo.cs
@@ -5,14 +5,14 @@ using UnityEngine;
 public class Ammo : MonoBehaviour
 {
     public int ammo = 10;
-
+    public int weaponIndex = 1;
     public GameObject ammoSound;
 
     private void OnTriggerEnter(Collider other)
     {
         if(other.tag.Equals("Player"))
         {
-            other.transform.root.GetComponentInChildren<WeaponController>().Ammo(10);
+            other.transform.root.GetComponent<WeaponManager>().TakeAmmo(ammo, weaponIndex);
 
             GameObject g = Instantiate(ammoSound, transform.position, Quaternion.identity);
             Destroy(g, 2);
---
+++ Assets/Scripts/WeaponManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManager : MonoBehaviour
+{
+    public GameObject[] weapons;
+    public float swapDelay = 0.5f;
+
+    public int selectedWeapon;
+
+    private float swapTimer;
+
+    private void Start()
+    {
+        selectedWeapon = 0;
+    }
+
+    private void Update()
+    {
+        if(Time.time > swapTimer)
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+                SwapWeapon(0);
+            else if (Input.GetKeyDown(KeyCode.Alpha2))
+                SwapWeapon(1);
+        }
+    }
+
+    private void SwapWeapon(int selection)
+    {
+        weapons[selectedWeapon].SetActive(false);
+        selectedWeapon = selection;
+        weapons[selectedWeapon].SetActive(true);
+
+        swapTimer = Time.time + swapDelay;
+    }
+
+    public void TakeAmmo(int count, int weaponIndex)
+    {
+        weapons[weaponIndex].GetComponent<WeaponController>().Ammo(count);
+    }
+}
