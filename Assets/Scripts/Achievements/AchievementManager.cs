fd5240376231eb0aef5c6472b9c222bb4cfdc330
laudienp <paul.laudien@etu.unice.fr>
2021-03-03 22:22:13 +0100

    add achievement manager

---
+++ Assets/Scripts/Achievements/AchievementManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AchievementManager : MonoBehaviour
+{
+    public GameObject panel;
+    public Text achievementText;
+
+    public void UnlockAchievement(string achievement) // add persistency
+    {
+        achievementText.text = achievement;
+        panel.SetActive(true);
+
+        Invoke("HidePanel", 5);
+    }
+
+    private void HidePanel()
+    {
+        panel.SetActive(false);
+    }
+}
