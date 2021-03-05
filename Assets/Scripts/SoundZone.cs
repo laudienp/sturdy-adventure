a89c806031b2d1e87c400a62386bd595251b763a
laudienp <paul.laudien@etu.unice.fr>
2021-03-05 12:32:54 +0100

    add sound zone

---
+++ Assets/Scripts/SoundZone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundZone : MonoBehaviour
+{
+    private AudioSource ambientSound;
+
+    private void Start()
+    {
+        ambientSound = GetComponent<AudioSource>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+            ambientSound.Play();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+            ambientSound.Stop();
+    }
+}
