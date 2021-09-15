12012b41621d60f7942b4c7b4925cec5cc607c44
laudienp <paul.laudien@etu.unice.fr>
2021-09-15 18:24:04 +0200

    update interaction input
    interaction input

---
+++ Assets/Inputs/InputMaster.cs
@@ -0,0 +1,132 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/Inputs/InputMaster.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputMaster : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputMaster()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputMaster"",
+    ""maps"": [
+        {
+            ""name"": ""Player"",
+            ""id"": ""1538b174-dd65-4acf-b8d0-260c4ca8914c"",
+            ""actions"": [
+                {
+                    ""name"": ""Interaction"",
+                    ""type"": ""Button"",
+                    ""id"": ""9ffb4ec2-cd3b-47fa-9b61-c948ff45c0b2"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""3eba1606-b6c2-486e-bf17-22334ce8b7fb"",
+                    ""path"": ""<Keyboard>/e"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Interaction"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": []
+}");
+        // Player
+        m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
+        m_Player_Interaction = m_Player.FindAction("Interaction", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Player
+    private readonly InputActionMap m_Player;
+    private IPlayerActions m_PlayerActionsCallbackInterface;
+    private readonly InputAction m_Player_Interaction;
+    public struct PlayerActions
+    {
+        private @InputMaster m_Wrapper;
+        public PlayerActions(@InputMaster wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Interaction => m_Wrapper.m_Player_Interaction;
+        public InputActionMap Get() { return m_Wrapper.m_Player; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(PlayerActions set) { return set.Get(); }
+        public void SetCallbacks(IPlayerActions instance)
+        {
+            if (m_Wrapper.m_PlayerActionsCallbackInterface != null)
+            {
+                @Interaction.started -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInteraction;
+                @Interaction.performed -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInteraction;
+                @Interaction.canceled -= m_Wrapper.m_PlayerActionsCallbackInterface.OnInteraction;
+            }
+            m_Wrapper.m_PlayerActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Interaction.started += instance.OnInteraction;
+                @Interaction.performed += instance.OnInteraction;
+                @Interaction.canceled += instance.OnInteraction;
+            }
+        }
+    }
+    public PlayerActions @Player => new PlayerActions(this);
+    public interface IPlayerActions
+    {
+        void OnInteraction(InputAction.CallbackContext context);
+    }
+}
--- Assets/Scripts/PlayerInteraction.cs
+++ Assets/Scripts/PlayerInteraction.cs
@@ -12,31 +12,48 @@ public class PlayerInteraction : MonoBehaviour
 
     public Text blockedMessageText;
 
+    private Interactible interactible;
+    private bool interaction;
+    private InputMaster input;
+
+    private void Awake()
+    {
+        input = new InputMaster();
+        input.Player.Interaction.performed += ctx => Interaction();
+    }
+
+    private void OnEnable() => input.Enable();
+
+    private void OnDisable() => input.Disable();
+
     void Update()
     {
         Ray center = Camera.main.ScreenPointToRay(new Vector3(Screen.width / 2, Screen.height / 2));
         
         if(Physics.Raycast(center, out RaycastHit hit, interactionRange) && hit.transform.CompareTag("Interactible"))
         {
-            Interactible interactible = hit.transform.GetComponent<Interactible>();
+            interactible = hit.transform.GetComponent<Interactible>();
             interactionText.text = interactible.interactionMessage;
-
-            if(Input.GetKeyDown(KeyCode.E))
-            {
-                if (!interactible.blocked)
-                    interactible.OnInteract.Invoke();
-                else
-                {
-                    SayBlockedMessage(interactible.blockedMessage);
-                    interactible.OnInteractBlocked.Invoke();
-                }
-                    
-            }
-                
+            interaction = true;
         }
         else
         {
             interactionText.text = "";
+            interaction = false;
+        }
+    }
+
+    private void Interaction()
+    {
+        if (!interaction)
+            return;
+        
+        if (!interactible.blocked)
+            interactible.OnInteract.Invoke();
+        else
+        {
+            SayBlockedMessage(interactible.blockedMessage);
+            interactible.OnInteractBlocked.Invoke();
         }
     }
 
